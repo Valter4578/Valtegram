36ea2511b760e3d827ab6f562b0d47d43874a634
Максим Алексеев <mrvalter566@gmail.com>
2020-03-08 14:56:07 +0700

    Start rewriting application

---
+++ Valtegram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Valtegram
+//
+//  Created by Максим Алексеев on 08.03.2020.
+//  Copyright © 2020 Максим Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
