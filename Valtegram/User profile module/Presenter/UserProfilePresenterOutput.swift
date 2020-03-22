f8d8afca961e9e152e2fb271b30f082dd8a22ed1
Максим Алексеев <mrvalter566@gmail.com>
2020-03-22 19:35:08 +0700

    Start rewrite profile vc to mvp

---
+++ Valtegram/User profile module/Presenter/UserProfilePresenterOutput.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfilePresenterOutput.swift
+//  Valtegram
+//
+//  Created by Максим Алексеев on 22.03.2020.
+//  Copyright © 2020 Максим Алексеев. All rights reserved.
+//
+
+import Foundation
