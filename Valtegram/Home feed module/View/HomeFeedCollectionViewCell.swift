07c5ac6e01e3480ff91320a2f3b4caaa49605eac
Максим Алексеев <mrvalter566@gmail.com>
2020-03-28 15:38:49 +0700

    Home feed module created

--- Valtegram/Home feed module/View/HomeFeedCollectionViewCell.swift
+++ Valtegram/Home feed module/View/HomeFeedCollectionViewCell.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 Максим Алексеев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HomePostCollectionViewCell: UICollectionViewCell {
+    // MARK:- Properties
+    var post: Post? {
+        didSet {
+            guard let post = post, let url = URL(string: post.imageUrl) else { return }
+            photoImageView.kf.setImage(with: url)
+        }
+    }
+    
+    // MARK:- Views
+    let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .cyan
+        return imageView
+    }()
+    // MARK:- Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK:- Setups
+    private func setupImageView() {
+        addSubview(photoImageView)
+        
+        NSLayoutConstraint.activate([
+            photoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            photoImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 0),
+            photoImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: 0),
+            photoImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+        ])
+    }
+    
+    
+}
